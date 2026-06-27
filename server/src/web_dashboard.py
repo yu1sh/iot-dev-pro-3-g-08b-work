bc37b611a3c80a09f750b0c96f0bc12ff36c5798
mochaji <249396231+mochaji@users.noreply.github.com>
2026-06-27 17:38:12 +0900

    fix: ログを追加し、Flaskサーバーの起動時およびダッシュボードリクエスト時の情報を記録

--- server/src/web_dashboard.py
+++ server/src/web_dashboard.py
@@ -6,15 +6,19 @@ from datetime import datetime
 from pathlib import Path
 
 from flask import Flask, render_template, send_file
+from logger_setup import setup_logger
 
 app = Flask(__name__)
+logger = setup_logger(__name__)
 
 CSV_DIR = Path(__file__).parent.parent / "outputs"
 CSV_FILE = CSV_DIR / "sensor_readings.csv"
+f_host = '0.0.0.0'
+f_port = 5001
 
 @app.route("/", methods=["GET"])
 def index():
-    print("Start")
+    logger.info("Dashboard request received")
 
     with open(CSV_FILE, newline="") as f:
         csv_data = list(csv.reader(f))
@@ -23,10 +27,12 @@ def index():
 
 @app.route('/files')
 def download():
+    logger.info("Download CSV")
     dt = datetime.now().strftime("%Y%m%d%H%M%S")
     file_name = f"sensor_readings_{dt}.csv"
     return send_file(CSV_FILE, as_attachment=True, download_name=file_name)
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5001, debug=True)
+    logger.info("Start flask server. host=%s, port %s", f_host, f_port)
+    app.run(host = f_host, port = f_port, debug=True)
