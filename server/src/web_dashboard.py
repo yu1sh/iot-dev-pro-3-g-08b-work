04d5b11a42759768e6da14173a3b18cf06b6dbf6
yuiti-oisi <yuichiro.tt02@gmail.com>
2026-07-03 17:34:03 +0900

    feat: 環境変数からサーバーのホストとポート番号を取得するように変更

--- server/src/web_dashboard.py
+++ server/src/web_dashboard.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env /usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import os
 import csv
 from datetime import datetime
 from pathlib import Path
+from dotenv import load_dotenv
 
 from flask import Flask, render_template, send_file
 from logger_setup import setup_logger
@@ -11,10 +13,12 @@ from logger_setup import setup_logger
 app = Flask(__name__)
 logger = setup_logger(__name__)
 
+load_dotenv(Path(__file__).with_name(".env"), verbose=True)
+
 CSV_DIR = Path(__file__).parent.parent / "outputs"
 CSV_FILE = CSV_DIR / "sensor_readings.csv"
-F_HOST = '0.0.0.0'
-F_PORT = 5001
+F_HOST = os.environ.get('F_HOST')
+F_PORT = int(os.environ.get('F_PORT'))
 
 @app.route("/", methods=["GET"])
 def index():
